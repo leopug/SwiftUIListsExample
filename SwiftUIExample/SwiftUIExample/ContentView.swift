0d51d6a1bfc2004894bfd178286d0954c5786fb8
leonardo maia pugliese <leo.pug@hotmail.com>
2019-10-14 22:01:26 -0300

    the commit
    agora vai

---
+++ SwiftUIExample/SwiftUIExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIExample
+//
+//  Created by Ana Caroline de Souza on 14/10/19.
+//  Copyright © 2019 Ana e Leo Corp. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
